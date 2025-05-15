517dac106a41d55bc8ab40ad8928788e1c8040c8
Arjun Bector <arjunbector@gmail.com>
2025-05-15 17:27:32 +0530

    feat:add navbar and dark theme

---
+++ app/(main)/navbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/globe.svg";
+import { UserButton } from "@clerk/nextjs";
+import { CreditCardIcon } from "lucide-react";
+import ThemeToggle from "@/components/ui/theme-toggle";
+import {dark} from "@clerk/themes"
+import { useTheme } from "next-themes";
+
+export default function Navbar() {
+  const {theme} = useTheme();
+  return (
+    <header className="shadow-sm">
+      <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
+        <Link href="resumes" className="flex items-center gap-2">
+          <Image
+            src={logo}
+            alt="logo"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+          <span className="text-xl font-bold tracking-tight">
+            Resume Builder
+          </span>
+        </Link>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
+              },
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                href="/billing"
+                label="Billing"
+                labelIcon={<CreditCardIcon className="size-4" />}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
+      </div>
+    </header>
+  );
+}
